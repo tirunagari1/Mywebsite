2055c8cf338e983001bf1db3f27387d2cc2dad92
tirunagari1 <38136589+tirunagari1@users.noreply.github.com>
2018-04-06 11:10:42 -0500

    Initial commit

---
+++ html/JS/Login.js
@@ -0,0 +1,11 @@
+function check(form){
+	if (form.userid.value == "srujan" && form.Pswd.value== "123") {
+		window.open("Home.html")
+
+	}
+	else
+	{
+		alert(" Incorrect USERNAME or PASSWORD")
+	}
+}
+
